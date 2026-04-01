8626ce3ccc13598fa8e88d191c8d383c6a18f686
pablocruzg <pablo.cruz.g68@gmail.com>
2026-04-01 10:01:46 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
+// Import Express.js
+const express = require('express');
+
+// Create an Express app
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Set port and verify_token
+const port = process.env.PORT || 3000;
+const verifyToken = process.env.VERIFY_TOKEN;
+
+// Route for GET requests
 app.get('/', (req, res) => {
   const mode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
 
-  // 🔍 log para ver qué llega
-  console.log("QUERY:", req.query);
-
-  if (mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
+  if (mode === 'subscribe' && token === verifyToken) {
     console.log('WEBHOOK VERIFIED');
     return res.status(200).send(challenge);
   }
 
-  // 👇 IMPORTANTE: no bloquear pruebas
-  res.send('Webhook activo');
+  // 👇 IMPORTANTE: no bloquear
+  res.send('OK');
+});
+
+// Route for POST requests
+app.post('/', (req, res) => {
+  const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
+  console.log(`\n\nWebhook received ${timestamp}\n`);
+  console.log(JSON.stringify(req.body, null, 2));
+  res.status(200).end();
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`\nListening on port ${port}\n`);
 });
