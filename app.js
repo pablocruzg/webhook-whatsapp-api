48647cb3a57df88b8e4dd2de14539b512d5bec14
Pablo Cruz G. <pablo.cruz.g68@gmail.com>
2026-04-10 12:09:19 -0600

    Cierre de conversación

--- app.js
+++ app.js
@@ -108,8 +108,9 @@ app.post('/', async (req, res) => {
 		let status_siguiente;
 		if (respuestaMenu) {
 			//status_siguiente = await getAccionDeOpcionMenu(ID_BOT, status_anterior, mensaje);	
+   		console.log('status_actual = await getAccionDeOpcionMenu(', ID_BOT, ' , ', status_anterior, ' , ' , mensaje);
 			status_actual = await getAccionDeOpcionMenu(ID_BOT, status_anterior, mensaje);	
-   		console.log(status_actual, 'await getAccionDeOpcionMenu(', ID_BOT, ' , ', status_anterior, ' , ' , mensaje);
+			console.log('status_actual = ', status_actual);
 			//status_anterior = status_actual;
 		} else {			
 			status_siguiente = await getSiguienteEstado(ID_BOT, status_actual);		
